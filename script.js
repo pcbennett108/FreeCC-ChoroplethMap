4850375880b8bab0784f26b83ca0eb52e9ea3a6e
pcbennett108 <pcbennett42@gmail.com>
2022-10-22 12:50:17 -0600

    Add mouseover tooltip and data-edu property for UserStories 10 & 11

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let countyData;
 let educationData;
 
 let canvas = d3.select("#canvas");
+let tooltip = d3.select("#tooltip");
 
 let drawMap = () => {
   canvas
@@ -44,6 +45,29 @@ let drawMap = () => {
       });
       let percentage = county["bachelorsOrHigher"];
       return percentage;
+    })
+    .on("mouseover", (countyDataItem) => {
+      tooltip.transition().style("visibility", "visible");
+
+      let id = countyDataItem["id"];
+      let county = educationData.find((item) => {
+        return item["fips"] === id;
+      });
+
+      tooltip.text(
+        county["fips"] +
+          " - " +
+          county["area-name"] +
+          ", " +
+          county["state"] +
+          " : " +
+          county["bachelorsOrHigher"] +
+          "%"
+      );
+      tooltip.attr("data-education", county["bachelorsOrHigher"]);
+    })
+    .on("mouseout", (countyDataItem) => {
+      tooltip.transition().style("visibility", "hidden");
     });
 };
 
